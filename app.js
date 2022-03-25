381764891496ba918e10825dd227b1b2b8bff44c
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-25 13:02:35 +0800

    增加 index.hbs 檔並編寫

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
+const roleData = require('./role.json')
 const app = express()
 const port = 3000
 
@@ -9,10 +9,16 @@ app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
  app.get('/', (req, res) => {
-   res.render('index')
+   res.render('index', { role: roleData})
  })
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+  res.render("index", { role: roleData });
+});
 
 app.listen(port, () => {
   console.log(`The web is running on http://localhost:${port}`)
