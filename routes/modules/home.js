cc0003e5092c475e460088930c8af45b17a5e9ac
Steavn Chen <tom9876555@yahoo.com.tw>
2022-10-12 02:39:13 +0800

    刪除不必要的CODE

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const roleData = require("../../role.json");
-const { getDryTalk } = require("../../tools/drytalk-generator.js")
+const express = require('express')
+const router = express.Router()
+const roleData = require('../../role.json')
+const { getDryTalk } = require('../../tools/drytalk-generator.js')
 
- router.get("/", (req, res) => {
-   res.render("index", { role: roleData });
- });
+router.get('/', (req, res) => {
+  res.render('index', { role: roleData })
+})
 
- router.post("/", (req, res) => {
-   const dryTalk = getDryTalk(req.body);
-   let disMessage;
-   if (req.body.role === undefined) {
-     disMessage = {
-       message: "error",
-       hint: "請選擇職業再送出 !",
-     };
-     return res.render("index", { role: roleData, disMessage: disMessage });
-   }
-   disMessage = {
-     message: "success",
-     hint: "多麼完美的幹話 !",
-   };
-   res.render("index", {
-     role: roleData,
-     dryTalk,
-     tasker: req.body.role,
-     disMessage: disMessage,
-   });
- });
+router.post('/', (req, res) => {
+  const dryTalk = getDryTalk(req.body)
+  let disMessage
+  if (req.body.role === undefined) {
+    disMessage = {
+      message: 'error',
+      hint: '請選擇職業再送出 !'
+    }
+    return res.render('index', { role: roleData, disMessage })
+  }
+  disMessage = {
+    message: 'success',
+    hint: '多麼完美的幹話 !'
+  }
+  res.render('index', {
+    role: roleData,
+    dryTalk,
+    tasker: req.body.role,
+    disMessage
+  })
+})
 
-module.exports = router;
+module.exports = router
